c0420193859edc1c88e0b1df6577fb0f864c9038
YordanGergov <yordan.gergov91@gmail.com>
2015-12-02 02:00:48 +0200

    mazalqk

--- MyAi/GoshoTheBot.cs
+++ MyAi/GoshoTheBot.cs
@@ -1,7 +1,7 @@
 ﻿namespace TexasHoldem.AI.SmartPlayer
 {
     using System;
-
+    using System.Collections.Generic;
     using TexasHoldem.AI.SmartPlayer.Helpers;
     using TexasHoldem.Logic;
     using TexasHoldem.Logic.Extensions;
@@ -17,29 +17,47 @@
         public static bool hasTheButton = false;
 
         //we should find who has iniative
-        public static bool iniative;
+        public static bool iniative = false;
 
         ////we should find if we are in a 3bettedPot
         public static bool is3bettedPot = false;
 
         public override string Name { get; } = "Gosho" + Guid.NewGuid();
-        
+
+        public override void StartGame(StartGameContext context)
+        {            
+            base.StartGame(context);
+        }
+
+        public override void StartHand(StartHandContext context)
+        {
+            is3bettedPot = false;
+            hasTheButton = false;
+            mFactor = context.MoneyLeft / (context.SmallBlind * 2);
+            base.StartHand(context);
+        }
+
+        public override void StartRound(StartRoundContext context)
+        {
+            
+            base.StartRound(context);
+        }
         public override PlayerAction GetTurn(GetTurnContext context)
         {
+        
             #region PreFlopLogic            
             if (context.RoundType == GameRoundType.PreFlop)
             {
-                hasTheButton = false;
                 //checks if we have the button
                 if (context.MyMoneyInTheRound == context.SmallBlind)
                 {
                     hasTheButton = true;
                 }
-
-                //calcutes M factor
-                // not sure if MoneyLeft property is OUR money left?!?!
-                mFactor = context.MoneyLeft / (context.SmallBlind * 2) ;
-               
+                //checks if 3bettedPot; If the pot is big it is!
+                if (context.CurrentPot > (context.SmallBlind*4))
+                {
+                    is3bettedPot = true;
+                }
 
                 var playHand = HandStrengthValuation.PreFlop(this.FirstCard, this.SecondCard, hasTheButton);
 
@@ -51,6 +69,7 @@
 
                 //push or fold if under 20 blinds
                 //http://www.holdemresources.net/h/poker-theory/hune/usage.html
+
                 if (mFactor < 20)
                 {
                     double nashEquillibriumRatio = 0;
@@ -65,7 +84,7 @@
                     }
 
                     //find if we have less money then the effective stack size
-                    bool push = context.MoneyLeft <= (nashEquillibriumRatio * context.SmallBlind);
+                    bool push = context.MoneyLeft <= (nashEquillibriumRatio * (context.SmallBlind* 2));
 
                     if (push)
                     {
@@ -103,7 +122,7 @@
                     return PlayerAction.Raise(context.SmallBlind * 6);
                 }
                 // folds if not in position
-                else if(playHand == CardValuationType.Risky && context.MyMoneyInTheRound != context.SmallBlind)
+                else if(playHand == CardValuationType.Risky && !hasTheButton)
                 {
                     
                     if (context.CanCheck)
@@ -115,7 +134,7 @@
                         return PlayerAction.Fold();
                     }
                 }
-
+              
                 if (playHand == CardValuationType.Recommended)
                 {
                    // var smallBlindsTimes = RandomProvider.Next(6, 10);
@@ -128,6 +147,15 @@
 
             // This doesn't make a difference if it is FLOP, TURN or RIVER!
             #region Post-FlopLogic 
+
+      //i don't know what I am doing! Trying to get who has initiative; THIS CRASHES!
+          //  List<string> lastActions = (List<string>)context.PreviousRoundActions;
+          //  var lastPlayer = lastActions[lastActions.Count];
+          //  if (lastPlayer == "Gosho Raise")
+          //  {
+          //      iniative = true;
+          //  }
+          
             //always call SmallBlinds!
             if (context.MoneyToCall == context.SmallBlind)
             {
@@ -140,11 +168,18 @@
             {
                 return PlayerAction.Raise(ourCurrentStack);
             }
-            
+
+           
             var chanceForAction = RandomProvider.Next(1, 50);
 
             double currentPotRaise = context.CurrentPot * 0.55;
             int currentPotRaiseInt = (int)currentPotRaise;
+
+            //always bets
+            if (iniative && context.CanCheck)
+            {
+                return PlayerAction.Raise(currentPotRaiseInt);
+            }
 
             if (chanceForAction <= 35)
             {
